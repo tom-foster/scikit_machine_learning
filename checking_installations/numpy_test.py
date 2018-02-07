f0d79185de30bd757d99e8a5ddf104ce19503d19
tom-foster <19tfoster87@gmail.com>
2018-02-07 22:18:30 +0000

    adding simple test files

---
+++ checking_installations/numpy_test.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#check that numpy is installed
+x = np.array([[1,2,3], [4,5,6]])
+print("x:\n{}".format(x))
+
+## switch to your console to check that it runs, by executing
+## python checking_installation/numpy_test.py
