00bec394aa0f84456047efc064aa6d81e65be1e7
tom-foster <19tfoster87@gmail.com>
2018-02-08 22:34:33 +0000

    updated, will expand tests later, if needed

--- tests/numpy_test.py
+++ tests/numpy_test.py
@@ -3,6 +3,7 @@ import numpy as np
 #check that numpy is installed
 x = np.array([[1,2,3], [4,5,6]])
 print("x:\n{}".format(x))
+print("Are you seeing a multi-dimensional array? Yes? - Success")
 
 ## switch to your console to check that it runs, by executing
 ## python checking_installation/numpy_test.py
